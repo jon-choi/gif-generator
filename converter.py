745879498011c7b4080e6294b3e0b6b8f5ca86d0
Jonathan Choi <jonchoi11@gmail.com>
2021-10-12 23:30:24 -0600

    finish gif generator

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import imageio
 import os
 
-clip = os.path.abspath('letternenny-clip.mp4')
+clip = os.path.abspath('letterkenny-clip.mp4')
 
 
 def gifMaker(inputPath, targetFormat):
@@ -10,3 +10,15 @@ def gifMaker(inputPath, targetFormat):
     print(f'converting {inputPath} \n to {outputPath}')
 
     reader = imageio.get_reader(inputPath)
+
+    fps = reader.get_meta_data()['fps']
+
+    writer = imageio.get_writer(outputPath, fps=fps)
+
+    for frames in reader:
+        writer.append_data(frames)
+        print(f'Frame {frames}')
+    print('Done')
+    writer.close()
+
+    gifMaker(clip, '.gif')
